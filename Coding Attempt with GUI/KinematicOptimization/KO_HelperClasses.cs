6cbd3ed50153645a4f9d7b50aebb66b68888c5d3
Girish Radhakrishnan <girish.radhakrishnan@orion-racing.com>
2018-06-11 10:27:15 -0600

    ---Created a New Folder and Class to house the helper's of KO---

---
+++ Coding Attempt with GUI/KinematicOptimization/KO_HelperClasses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Attempt_with_GUI
+{
+    public class KO_HelperClasses
+    {
+
+    }
+
+    /// <summary>
+    /// Enum of <see cref="SuspensionParameters"/> for which the Suspension Coordinates would be Optimized for
+    /// </summary>
+    public enum SuspensionParameters
+    {
+        FrontRollCenter,
+        RearRollCenter,
+        LeftPitchCenter,
+        RightPitchCenter,
+        CamberVariation,
+        BumpSteer,
+        Ackermann,
+        SpringDeflection
+    }
+
+    public enum SuspensionParameterMode
+    {
+        Static,
+        Variation
+    }
+
+
+
+}
+
