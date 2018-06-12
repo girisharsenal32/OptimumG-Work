0ab5c7307a32782300cec61185ee69a157df8b9b
Girish Radhakrishnan <girish.radhakrishnan@orion-racing.com>
2018-06-12 10:52:43 -0600

    ---Added  1 comment that's it---

--- Coding Attempt with GUI/KO_CornverVariables.cs
+++ Coding Attempt with GUI/KO_CornverVariables.cs
@@ -9,7 +9,13 @@ namespace Coding_Attempt_with_GUI
 {
     public class KO_CornverVariables
     {
+        /// <summary>
+        /// <para>Master <see cref="Dictionary{String, KO_AdjToolParams}"/> which holds ALL the coordinate information of the Adjustable coordinates</para> 
+        /// <para>This dictionary is crucial and will be used in the Main Optimizer Class</para>
+        /// </summary>
         public Dictionary<string,KO_AdjToolParams> KO_MasterAdjs { get; set; }
+
+        
 
 
         public KO_CornverVariables()
